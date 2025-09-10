45f9dc6db90950e53e27eb82e44e55d5ba2d39bb
boblio-max <nikhilmahankali56@gmail.com>
2025-09-09 21:08:11 -0700

    Create backends.py

---
+++ quantum_leap/backends.py
@@ -0,0 +1,10 @@
+"""
+QuantumLeap: A Python-based Quantum Computing Emulator.
+
+This package provides the core components for building and simulating quantum circuits.
+"""
+from .circuit import QuantumCircuit
+from .visualizer import plot_histogram
+
+__version__ = "1.0.0"
+__author__ = "Nikhil Mahankali"
